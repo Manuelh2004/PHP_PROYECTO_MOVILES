6580e9eb444683e2c787136e7398b3344a75dba1
Gonzalo <gohecaze@gmail.com>
2025-06-15 01:17:57 -0500

    Modificacion con el llenado de categoria presupuesto si ya existe

--- controladores/presupuestoController/obtener_categorias.php
+++ controladores/presupuestoController/obtener_categorias.php
@@ -4,8 +4,13 @@ require_once("../../configuracion/conexion.php");
 
 $con = conectar();
 
-$sql = "SELECT id_categoria, nom_categoria FROM categoria";
-$resultado = $con->query($sql);
+$usuario = $_GET['id_usuario']; // o como lo pases en la solicitud
+
+$stmt = $con->prepare("SELECT id_categoria, nom_categoria FROM categoria WHERE id_categoria NOT IN (SELECT id_categoria FROM presupuesto WHERE id_usuario = ?)");
+$stmt->bind_param("i", $usuario);
+$stmt->execute();
+
+$resultado = $stmt->get_result();
 
 $categorias = array();
 while ($fila = $resultado->fetch_assoc()) {
