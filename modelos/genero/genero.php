96115a99838dbe924b14fd8dccdaf8ebe0a5a820
abraham <abraham2004hf@gmail.com>
2025-06-15 20:29:22 -0500

    Avance del modulo adm

--- modelos/genero/genero.php
+++ modelos/genero/genero.php
@@ -24,4 +24,34 @@
 
         return $datos;
     }
+
+    function RegistrarGenero($nom_genero){
+        require_once("C:/wamp64/www/proyecto_moviles/configuracion/conexion.php");
+        $con = conectar();
+
+        // Para evitar SQL Injection, se recomienda preparar la sentencia.
+        $sql = "INSERT INTO genero (nom_genero)
+                VALUES (?)";
+
+        $stmt = mysqli_prepare($con, $sql);
+
+        if (!$stmt) {
+            return "Error en la preparación de la consulta";
+        }
+
+        mysqli_stmt_bind_param($stmt, "s", $nom_categoria);
+
+        $exec = mysqli_stmt_execute($stmt);
+
+        if (!$exec) {
+            $msg = "Error al registrar el genero";
+        } else {
+            $msg = "success";
+        }
+
+        mysqli_stmt_close($stmt);
+        mysqli_close($con);
+
+        return $msg;
+    }
 ?>
