ab7024bdf7fca59cc95c1c8096e6d8eb0f001e71
abraham <abraham2004hf@gmail.com>
2025-07-07 22:08:39 -0500

    Cambios

--- configuracion/conexion.php
+++ configuracion/conexion.php
@@ -1,5 +1,5 @@
 <?php
 function conectar() {
-    return mysqli_connect("localhost", "root","", "bd_nueva", "3306");
+    return mysqli_connect("localhost", "root","", "bd_proyecto_moviles", "3306");
 }
 ?>
